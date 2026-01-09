6e6f31c7576625548a27ae96a006bd3711d21e22
minkyu_Park <kyle970320@farmzonesnc.com>
2026-01-09 16:51:56 +0900

    feat: modify path in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from 'node:url'
 import path from 'node:path'
-
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
